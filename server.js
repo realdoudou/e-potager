1bccce629a2ac1a372d6d87f3ea8c72f57fc3669
EXT.GCERUTTI <EXT.GCERUTTI@FRMRSHODT6316>
2017-05-23 14:49:08 +0200

    ajout node serveur

---
+++ server.js
@@ -0,0 +1,12 @@
+﻿let express = require('express')
+let open = require('open')
+let port = 3000
+let urlToLaunch = "http://localhost:3000/popo.html"
+open(urlToLaunch)
+
+
+let app = express()
+app.use('/', express.static(__dirname))
+app.listen(port, function () {
+	console.log('listening on '+urlToLaunch)
+})
